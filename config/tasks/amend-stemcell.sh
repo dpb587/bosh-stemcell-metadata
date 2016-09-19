b6a85a5d9c12a8b68f034363267e62261a144d9b
Danny Berger <dpb587@gmail.com>
2016-09-18 22:53:57 -0700

    initial commit

---
+++ config/tasks/amend-stemcell.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -eu
+
+git clone file://$PWD/gh-pages gh-pages-output
+
+cd gh-pages-output
+
+if [ ! -e "$stemcell.json" ]; then
+  echo '[]' > "$stemcell.json"
+fi
+
+version=$( cat ../stemcell/version )
+
+# build our record
+
+jq -n \
+  --arg version "$version" \
+  --slurpfile metadata ../stemcell-metadata/.datapact/result \
+  '
+    {
+      "version": $version,
+      "metadata": (
+        $metadata[0].output
+        | map({
+          "key": ( .path | gsub("\.gz$", "") ),
+          "value": (
+            . +
+            {
+              "url": (env.s3_endpoint + "/" + env.s3_bucket + "/" + env.s3_prefix + $metadata.branch + "/" + .blob)
+            }
+          )
+        })
+        | from_entries
+      )
+    }
+  ' \
+  > /tmp/amend.json
+
+# remove any existing versions
+
+jq \
+  --slurpfile amend /tmp/amend.json \
+  '
+    map(select(.version != $amend.version))
+    + $amend
+    | sort_by($amend.version | split("."))
+  ' \
+  < "$stemcell.json" \
+  > "$stemcell.json.new"
+
+cp "$stemcell.json.new" "$stemcell.json"
+
+git add "$stemcell.json"
+git commit -m "Update $stemcell.json" "$stemcell.json"
